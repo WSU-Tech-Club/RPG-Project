32f29cdbab260bc6ed265d598410c65e006c3470
whyrusleeping <jeromyj@gmail.com>
2012-06-11 15:16:22 -0700

    Ideas on consumables

--- consumable.h
+++ consumable.h
@@ -4,5 +4,13 @@
 
 class Consumable : public Item
 {
+private:
 	int uses;
+	int type; //maybe use enum for type?
+public:
+	Consumable();
+
+//My thoughts are that the consumable will have a 'Use' method that can be overloaded for different types of items.
+//for example, an item that would heal you would accept a pointer to a player object
+	
 };
