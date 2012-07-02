7a904d2cb4b605856c0dd54979b2fb2fbf5834ce
whyrusleeping <jeromyj@gmail.com>
2012-07-02 08:20:42 -0700

    Update master

--- npcAction.h
+++ npcAction.h
@@ -3,7 +3,7 @@
 //this class will be how npc will comunicate back to the world. its purpose will be to contain actions to be acted out throughout the world.
 //such as yelling in school might make your freinds want to not hang out with you or the school might call your house.
 
-#include"action.h"
+#include "action.h"
 
 class npcAction :public Action
 {
@@ -13,6 +13,7 @@ public:
 	virtual bool setAction(string fileName, Opinon nOpinon, Personality nPersonality);
 
 private:
+	void generateRepurcussions();
 
 
 };
